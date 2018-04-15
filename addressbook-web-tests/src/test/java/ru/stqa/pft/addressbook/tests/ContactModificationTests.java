70acacd486e90e0069ce81ae965b34ba7b293a51
konfast <konfast@gmail.com>
2018-04-15 13:10:37 +0300

    Реализована проверка и обеспечение выполнения предусловий в тесте модификации контактов в адресной книге

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,6 +7,10 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    if (! app.getContactHelper().isThereContact()) {
+      app.getContactHelper().createContact(new ContactData("Svetlana", "Ivanova", "Ukraine", "111-11-11", "ivanova@localhost.com", "test1"), true);
+      app.getNavigationHelper().returnToHomePage();
+    }
     app.getContactHelper().initContactModification();;
     app.getContactHelper().fillContactCreationForm(new ContactData("Elena", "Petrova", "Russia", "222-22-22", "petova@localhost.com", null), false);
     app.getContactHelper().summitContactModification();
