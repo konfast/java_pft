fa84a93b9bf6722366278dbfea1e848af4b364c5
konfast <konfast@gmail.com>
2018-05-22 20:29:59 +0300

    Реализовано получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -36,7 +36,7 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
 
   private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -52,8 +52,11 @@ public class HttpSession {
     HttpGet get = new HttpGet(app.getProperty("web.baseURL") + "/index.php");
     CloseableHttpResponse response = httpclient.execute(get);
     String body = geTextFrom(response);
-    //return body.contains(String.format("<a href=\"\\/mantisbt-2.14.0\\/account_page.php\">%s</a>", username));
-    return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
+    //return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
+    //return body.contains(String.format("//span[contains(text(),'%s')]", username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
+
+
 
   }
 }
