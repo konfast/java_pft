cd9ad00df3618e239aea4f942b87c69fb0eea1b4
konfast <konfast@gmail.com>
2018-06-03 14:30:06 +0300

    Ошибка NullPointerException_1

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
@@ -9,21 +10,5 @@ import java.io.IOException;
 import static org.testng.Assert.assertTrue;
 
 public class ChangePasswordTests extends TestBase {
-
-  @BeforeMethod
-  public void startMailServer() {
-    app.mail().start();
-  }
-
-  @Test
-
-  public void testChangePassword() throws IOException {
-    HttpSession session = app.newSession();
-    session.login("administrator", "root");
-    assertTrue(session.isLoggedInAs("administrator"));
-    goToManage();
-
-  }
-
 
 }
